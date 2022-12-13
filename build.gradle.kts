e55d4266a2e8cbf586a8cf56b435ebdbd62e6f53
hosizoraru <hosizoraru@gmail.com>
2022-12-13 16:49:56 +0800

    kotlin 1.8.0-RC

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.0.0-alpha02")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0-Beta")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0-RC")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
