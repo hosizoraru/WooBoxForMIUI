84d83aa6744bc7429fa5a2ccd80a2daad798f024
hosizoraru <hosizoraru@gmail.com>
2022-12-22 17:22:56 +0800

    some try

--- app/src/main/java/com/lt2333/simplicitytools/hook/app/android/AllowSoundSourceBoth.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/android/AllowSoundSourceBoth.kt
@@ -1,5 +1,7 @@
 package com.lt2333.simplicitytools.hook.app.android
 
+import com.github.kyuubiran.ezxhelper.utils.findMethod
+import com.github.kyuubiran.ezxhelper.utils.hookBefore
 import com.lt2333.simplicitytools.util.XSPUtils
 import com.lt2333.simplicitytools.util.xposed.base.HookRegister
 
