7d754871d9661eea51d169acd8249ef429e965fc
hosizoraru <hosizoraru@gmail.com>
2022-12-28 15:04:39 +0800

    some try

---
+++ app/src/main/java/com/lt2333/simplicitytools/hook/app/android/GetMinimumSignatureSchemeVersionForTargetSdk.kt
@@ -0,0 +1,26 @@
+package com.lt2333.simplicitytools.hook.app.android
+
+import com.github.kyuubiran.ezxhelper.utils.findMethod
+import com.github.kyuubiran.ezxhelper.utils.hookMethod
+import com.lt2333.simplicitytools.util.xposed.base.HookRegister
+import de.robv.android.xposed.XposedBridge
+
+
+object GetMinimumSignatureSchemeVersionForTargetSdk : HookRegister() {
+    override fun init() {
+        try {
+            findMethod("android.util.apk.ApkSignatureVerifier") {
+                name == "getMinimumSignatureSchemeVersionForTargetSdk"
+            }.hookMethod {
+                after { param ->
+                    param.result = 0
+                }
+            }
+            XposedBridge.log("BypassCheck: Hook getMinimumSignatureSchemeVersionForTargetSdk success!")
+        } catch (e: Throwable) {
+            XposedBridge.log("BypassCheck: Hook getMinimumSignatureSchemeVersionForTargetSdk failed!")
+        }
+
+    }
+
+}
