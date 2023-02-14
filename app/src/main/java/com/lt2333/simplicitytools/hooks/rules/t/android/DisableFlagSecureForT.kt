ce29dc8bd6b0c357b0a634ecd179a3ba60f04927
hosizoraru <hosizoraru@gmail.com>
2023-02-14 17:47:45 +0800

    合并 f39d7d6 修复A13中部分场景无法强制截图
    f39d7d601d46b0c2c251a516d578f372833f5539
    https://github.com/Simplicity-Team/WooBoxForMIUI/commit/f39d7d601d46b0c2c251a516d578f372833f5539

--- app/src/main/java/com/lt2333/simplicitytools/hooks/rules/t/android/DisableFlagSecureForT.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hooks/rules/t/android/DisableFlagSecureForT.kt
@@ -1,7 +1,9 @@
 package com.lt2333.simplicitytools.hooks.rules.t.android
 
+import android.os.Build
 import com.github.kyuubiran.ezxhelper.utils.findMethod
 import com.github.kyuubiran.ezxhelper.utils.hookReturnConstant
+import com.github.kyuubiran.ezxhelper.utils.hookAllConstructorBefore
 import com.lt2333.simplicitytools.utils.hasEnable
 import com.lt2333.simplicitytools.utils.xposed.base.HookRegister
 
@@ -10,9 +12,22 @@ object DisableFlagSecureForT : HookRegister() {
         findMethod("com.android.server.wm.WindowState") {
             name == "isSecureLocked"
         }.hookReturnConstant(false)
+        
         findMethod("com.android.server.wm.WindowManagerService") {
             name == "isSecureLocked"
         }.hookReturnConstant(false)
+        
+        findMethod("com.android.server.wm.WindowSurfaceController") {
+                name == "setSecure"
+        }.hookBefore {
+            it.args[0] = false
+        }
+        hookAllConstructorBefore("com.android.server.wm.WindowSurfaceController") {
+            var flags = it.args[2] as Int
+            val secureFlag = 128
+            flags = flags and secureFlag.inv()
+            it.args[2] = flags
+        }
     }
     
 }
