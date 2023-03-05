a196bd48bde62a550957f37f9fe8526e2ca5da25
hosizoraru <hosizoraru@gmail.com>
2023-03-05 19:28:30 +0800

    修复 去除游戏自动连招黑名单

--- app/src/main/java/com/lt2333/simplicitytools/hooks/rules/all/securitycenter/RemoveMacroBlacklistForAll.kt
+++ app/src/main/java/com/lt2333/simplicitytools/hooks/rules/all/securitycenter/RemoveMacroBlacklistForAll.kt
@@ -2,6 +2,7 @@ package com.lt2333.simplicitytools.hooks.rules.all.securitycenter
 
 import com.github.kyuubiran.ezxhelper.utils.*
 import com.lt2333.simplicitytools.utils.XSPUtils
+import com.lt2333.simplicitytools.utils.hookMethod
 import com.lt2333.simplicitytools.utils.xposed.base.HookRegister
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
@@ -28,6 +29,20 @@ object RemoveMacroBlacklistForAll : HookRegister() {
                 }
                 letter++
             }
+//            findMethod("q7.m0","n",["java.util.ArrayList"]){
+//                name == "n" && parameterTypes[0] == java.util.ArrayList::class.java
+//            }.hookBefore {
+//                it.args[0] = '[]'
+//            }
+            findMethod("q7.m0") {
+                name == "n" && parameterTypes[0] == ArrayList::class.java
+            }.hookBefore { param ->
+                param.args[0] = "[]"
+            }
+
+            findMethod("q7.m0"){
+                name == "g" && parameterTypes[0] == String::class.java
+            }.hookReturnConstant(false)
         }
     }
 
